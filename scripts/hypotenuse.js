394408f69ab9097a423fbdf04226e5e4ce603a92
RakshitB07 <92932235+RakshitB07@users.noreply.github.com>
2022-09-05 13:19:45 +0530

    clean code

--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -1,6 +1,6 @@
-const sides = document.querySelectorAll(".side-input");
 const checkHypoBtn = document.querySelector("#check-hypo-btn");
 const outputEl = document.querySelector("#output");
+const sides = document.querySelectorAll(".side-input");
 
 function calculateSumOfSquare(a, b) {
     const sumOfSquares = a * a + b * b;
@@ -9,8 +9,8 @@ function calculateSumOfSquare(a, b) {
 
 function calculateHypo() {
     const sumOfSquares = calculateSumOfSquare(Number(sides[0].value), Number(sides[1].value));
-    const lengthOfHyypo = Math.sqrt(sumOfSquares);
-    outputEl.innerText = "Length of hypotenuse is " + lengthOfHyypo
+    const lengthOfHypo = Math.sqrt(sumOfSquares);
+    outputEl.innerText = "Length of hypotenuse is: " + lengthOfHypo
 
 
 
